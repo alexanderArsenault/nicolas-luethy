28d9f08d10601b8a3ab544587b3ff65ff082bed3
Alex <rsenaultc@gmail.com>
2019-12-01 19:15:18 +0100

    updated

--- src/views/Education.vue
+++ src/views/Education.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    educats
+  <div class="timeline-container">
+    <div
+      class="timeline-item"
+      v-for="(item, index) in timeline"
+      :key="`timline-item-${index}`"
+    >
+      <div class="timeline-left-container">
+        <div class="timeline-header">{{ item.header }}</div>
+        <div class="timeline-body">{{ item.body }}</div>
+        <div class="timeline-dates">
+          {{ item.datestart }} - {{ item.dateend }}
+        </div>
+      </div>
+      <div class="timeline-center"></div>
+      <div class="timeline-right-container">
+        <img :src="item.img" alt="" />
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    name: "timeline",
+    props: {
+      timeline: {
+        type: Array
+      }
+    }
+  };
+</script>
